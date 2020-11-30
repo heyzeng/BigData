afee31b780ced92eaf57db28209a175871493fb0
heyzeng <270806204@qq.com>
2020-11-30 14:10:43 +0800

    flinkwc

---
+++ flink/src/main/java/OnOfflineDevive.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+/**
+ * Author:Jude
+ * Date:2020-11-24 下午3:58
+ */
+public class OnOfflineDevive {
+
+
+    public static void main(String[] args) {
+
+        String[] results = {"lumi123","111","12345r35"};
+
+        ArrayList<String> resultList = new ArrayList<>();
+
+        String lastDid = null;
+        Long lastTime = null;
+        String lastData = null;
+        String lastValue = null;
+        String result = null;
+
+
+        for (String key : results){
+
+            String[] resValues = key.split(",");
+
+
+
+        }
+
+//        int[] ints = {1,1,2,3,3,3,3};
+//
+//        for (int i : ints){
+//            System.out.println(ints[i]);
+//        }
+    }
+}
---
+++ flink/src/main/java/com/wordcount/Flink01_WC_Batch.java
@@ -0,0 +1,49 @@
+package com.wordcount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * Author:Jude
+ * Date:2020-11-30 上午9:37
+ * 批处理
+ */
+public class Flink01_WC_Batch {
+
+    public static void main(String[] args) throws Exception {
+
+        // 拿到执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 获取数据源
+        DataSource<String> dataLine = env.readTextFile("/Users/judezeng/Desktop/GoodGoodStudy/BigData/hadoop/src/main/resources/word");
+
+        // 按照word分组
+        FlatMapOperator<String, Tuple2<String,Integer>> wordsAndOne = dataLine.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String [] words = value.split(" ");
+                for (String word : words){
+                    out.collect(new Tuple2<String,Integer>(word,1));
+                }
+            }
+        });
+
+        // word 分组
+        UnsortedGrouping<Tuple2<String,Integer>> wordGroup = wordsAndOne.groupBy(0);
+
+        //分组内聚合
+        AggregateOperator<Tuple2<String,Integer>> wordSum = wordGroup.sum(1);
+
+        //打印
+        wordSum.print();
+
+    }
+}
